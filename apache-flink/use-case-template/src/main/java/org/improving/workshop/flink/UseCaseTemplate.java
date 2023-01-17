f821170fcc108b7cf308cf3188a7ffa5dba667bd
Nick Larson <nicklas.larson@gmail.com>
2023-01-17 15:32:41 -0600

    initial flink and use-case template

---
+++ apache-flink/use-case-template/src/main/java/org/improving/workshop/flink/UseCaseTemplate.java
@@ -0,0 +1,16 @@
+package main.java.org.improving.workshop.flink;
+
+
+
+public class UseCaseTemplate {
+    
+    /**
+     * Main method.
+     *
+     * @throws Exception which occurs during job execution.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Use Case Template");
+    }
+
+}
---
+++ apache-flink/use-case-template/src/solution/java/org/improving/workshop/flink/UseCaseSolutionTemplate.java
@@ -0,0 +1,12 @@
+package solution.java.org.improving.workshop.flink;
+
+public class UseCaseSolutionTemplate {
+        /**
+     * Main method.
+     *
+     * @throws Exception which occurs during job execution.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Use Case Solution Template");
+    }
+}
